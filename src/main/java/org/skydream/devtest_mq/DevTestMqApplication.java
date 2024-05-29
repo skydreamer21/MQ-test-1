ee9d43b22c0fc49e5ee515438341fe4c9d10fecf
skydreamer <skydreamer210@gmail.com>
2024-05-29 16:05:07 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/org/skydream/devtest_mq/DevTestMqApplication.java
@@ -0,0 +1,13 @@
+package org.skydream.devtest_mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTestMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevTestMqApplication.class, args);
+    }
+
+}
